c07d31447f6d25f92ff89da609424055e349037b
J <codergirljp@gmail.com>
2014-09-09 19:29:02 -0500

    Adding closer to raw color data to protocol for future use
    Adding closer to raw color data to protocol for future use

--- jp.Comms/jp.Comms/ColorSensorProtocol.cs
+++ jp.Comms/jp.Comms/ColorSensorProtocol.cs
@@ -7,6 +7,10 @@
         public int B = 0;
         public string HexColor = "000000";
         public bool LightIsOn = true;
+        public int ClearK = 0;
+        public int RedK = 0;
+        public int GreenK = 0;
+        public int BlueK = 0;
 
         public void GetData(string message)
         {
@@ -18,6 +22,10 @@
                 B = System.Convert.ToInt32(msgSplit[3]);
                 HexColor = msgSplit[4].ToString();
                 LightIsOn = System.Convert.ToBoolean(System.Convert.ToInt32(msgSplit[5]));
+                ClearK = System.Convert.ToInt32(msgSplit[6]);
+                RedK = System.Convert.ToInt32(msgSplit[7]);
+                GreenK = System.Convert.ToInt32(msgSplit[8]);
+                BlueK = System.Convert.ToInt32(msgSplit[9]);
             }
         }
     }
